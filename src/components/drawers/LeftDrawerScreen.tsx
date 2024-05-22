5a22b3e1719c101c8a8b0ef23dfa74e255bf5ed0
Johan Román <r.johan95@gmail.com>
2024-05-22 11:52:29 -0400

    arreglar para colocar tipado correcto y colocar provider multilenguaje

--- src/components/drawers/LeftDrawerScreen.tsx
+++ src/components/drawers/LeftDrawerScreen.tsx
@@ -1,12 +1,15 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useTranslation } from "react-i18next";
 import Budgets from "../../screens/CMRSales/Budgets/Budgets";
 import HomeScreen from "../../screens/Home/Home";
 import CustomDrawer from "./CustomDrawer";
 import CreateBudget from "../../screens/CMRSales/Budgets/CreateBudget/CreateBudget";
+import Configs from "../../screens/Configs/Configs";
 
 const LeftDrawer = createDrawerNavigator();
 
 export default function LeftDrawerScreen() {
+    const { t } = useTranslation();
     return (
         <LeftDrawer.Navigator
             drawerContent={(props) => <CustomDrawer {...props} />}
@@ -14,16 +17,24 @@ export default function LeftDrawerScreen() {
                 drawerPosition: "left",
                 drawerStyle: {
                     backgroundColor: "#3C414F", // Cambia este valor al color que desees
-                    drawerInactiveTintColor: "#fff",
                 },
             }}
         >
-            <LeftDrawer.Screen name="home" component={HomeScreen} />
+            <LeftDrawer.Screen
+                name="home"
+                component={HomeScreen}
+                options={{ title: t("menu-title-home") }}
+            />
+            <LeftDrawer.Screen
+                name="configs"
+                component={Configs}
+                options={{ title: t("menu-title-config") }}
+            />
             <LeftDrawer.Screen name="createBudget" component={CreateBudget} />
             <LeftDrawer.Screen
                 name="budgets"
                 component={Budgets}
-                options={{ title: "Presupuestos" }}
+                options={{ title: t("menu-title-budgets") }}
             />
         </LeftDrawer.Navigator>
     );
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,7 +3,16 @@ import { Button, View } from "react-native";
 import { RightDrawerContext } from "../../components/drawers/RightDrawerScreen";
 
 export default function HomeScreen({ navigation }: { navigation: any }) {
-    const { openRightDrawer } = useContext(RightDrawerContext);
+    const contextValue = useContext(RightDrawerContext);
+
+    // Check if the context has been successfully retrieved
+    if (!contextValue) {
+        console.error("RightDrawerContext is not available");
+        return null; // Or return a loading indicator, etc.
+    }
+
+    // Now TypeScript knows that contextValue is of type RightDrawerContextType
+    const { toggleOpenRight } = contextValue;
 
     return (
         <View
@@ -13,10 +22,7 @@ export default function HomeScreen({ navigation }: { navigation: any }) {
                 onPress={() => navigation.openDrawer()}
                 title="Open left drawer"
             />
-            <Button
-                onPress={() => openRightDrawer()}
-                title="Open right drawer"
-            />
+            <Button onPress={() => toggleOpenRight()} title="Open right drawer" />
         </View>
     );
 }
