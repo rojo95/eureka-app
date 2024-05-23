0323ebd96955201c187ed0a63890a50f1899a2d1
Johan Román <r.johan95@gmail.com>
2024-05-22 20:32:45 -0400

    integracion de las fuentes

--- App.tsx
+++ App.tsx
@@ -5,15 +5,32 @@ import { I18nextProvider } from "react-i18next";
 import {
     MD3LightTheme as DefaultTheme,
     PaperProvider,
+    configureFonts,
 } from "react-native-paper";
 import RightDrawerScreen from "./src/components/drawers/RightDrawerScreen";
 import i18n from "./services/i18next";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import changeLanguage from "./src/utils/Language";
+import { Platform } from "react-native";
+
+const fontConfig: any = {
+    customVariant: {
+        fontFamily: Platform.select({
+            web: "Manrope",
+            ios: "System",
+            default: "Manrope",
+        }),
+        fontWeight: "400", // Change this line if needed based on the expected types
+        letterSpacing: 0.5,
+        lineHeight: 22,
+        fontSize: 20,
+    },
+};
 
 // custom theme
 const theme = {
     ...DefaultTheme,
+    fonts: configureFonts({ config: fontConfig }),
     // Specify custom property
     myOwnProperty: true,
     // Specify custom property in nested object
