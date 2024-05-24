c7eb04be72f2faa8fb529f3a86b05159f14220f2
Johan Román <r.johan95@gmail.com>
2024-05-24 18:58:29 -0400

    agregar funciones al FAB y corregir testeos e identacion

--- src/components/FAB/FAB.tsx
+++ src/components/FAB/FAB.tsx
@@ -1,76 +1,69 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import {
-  DefaultTheme,
-  FAB as FABButton,
-  Portal,
-  Provider,
-  useTheme,
+    DefaultTheme,
+    FAB as FABButton,
+    Portal,
+    Provider,
+    useTheme,
 } from "react-native-paper";
 
 interface FABInterface {
-  actions?: any[];
-  onOpen?: () => void;
-  onClose?: () => void;
+    actions?: any[];
+    onOpen?: () => void;
+    onClose?: () => void;
 }
+
 const FAB = ({ actions, onOpen, onClose }: FABInterface) => {
-  const theme: DefaultTheme = useTheme();
-  const [open, setOpen] = useState<boolean>(false);
+    const theme: DefaultTheme = useTheme();
+    const [open, setOpen] = useState<boolean>(false);
 
-  const onStateChange = (open: boolean) => setOpen(open);
+    const onStateChange = (open: boolean) => setOpen(open);
 
-  function openFunction(): void {
-    onOpen;
-  }
+    const openFunction = () => {
+        onOpen && onOpen();
+    };
 
-  function closeFunction(): void {
-    onclose;
-  }
+    const closeFunction = () => {
+        onClose && onClose();
+    };
 
-  const styles = StyleSheet.create({
-    container: {
-      position: "absolute",
-      left: 16,
-      bottom: 16,
-    },
-    fab: {
-      color: "#fff", // Color de fondo del FAB
-    },
-  });
+    const styles = StyleSheet.create({
+        container: {
+            position: "absolute",
+            left: 16,
+            bottom: 16,
+        },
+    });
 
-  // Si el FAB no está abierto y no hay acciones disponibles, no renderizar el componente del FAB
-  if (!open && (!actions || actions.length === 0)) {
-    return null;
-  }
-
-  return (
-    <Provider>
-      <View style={styles.container}>
-        <Portal>
-          <FABButton.Group
-            open={open}
-            visible
-            color={theme.colors.primaryContrast}
-            icon={open ? "clipboard-check-outline" : "plus"}
-            actions={actions || []}
-            fabStyle={{ backgroundColor: theme.colors.primary }}
-            backdropColor={"transparent"}
-            onStateChange={() => {
-              onStateChange(!open);
-            }}
-            onPress={() => {
-              if (open) {
-                openFunction();
-                // hacer algo si el grupo está abierto
-              } else {
-                closeFunction();
-              }
-            }}
-          />
-        </Portal>
-      </View>
-    </Provider>
-  );
+    return (
+        <Provider>
+            <View style={styles.container}>
+                <Portal>
+                    <FABButton.Group
+                        open={open}
+                        visible
+                        color={theme.colors.primaryContrast}
+                        icon={open ? "clipboard-check-outline" : "plus"}
+                        actions={actions || []}
+                        fabStyle={{ backgroundColor: theme.colors.primary }}
+                        backdropColor={"transparent"}
+                        onStateChange={() => {
+                            actions && onStateChange(!open);
+                        }}
+                        onPress={() => {
+                            if (open) {
+                                // hacer algo si el grupo está abierto
+                                closeFunction();
+                            } else {
+                                openFunction();
+                            }
+                        }}
+                    />
+                </Portal>
+            </View>
+        </Provider>
+    );
 };
 
 export default FAB;
