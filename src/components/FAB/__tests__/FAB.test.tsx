529e9e16a8752120755cc1e8d0052ef6049477dc
Johan Román <r.johan95@gmail.com>
2024-05-27 13:52:13 -0400

    eliminacion de codigo comentado

--- src/components/FAB/__tests__/FAB.test.tsx
+++ src/components/FAB/__tests__/FAB.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import renderer, { act } from "react-test-renderer";
 import FAB from "../FAB";
 
 describe("Test to evaluate <FAB /> component", () => {
@@ -7,21 +7,4 @@ describe("Test to evaluate <FAB /> component", () => {
         const tree: any = renderer.create(<FAB />).toJSON();
         expect(tree.children.length).toBe(2);
     });
-
-    // it("should render 5 elements", () => {
-    //     const elements /* : Item[] */ = [...Array(5)].map((d, index) => {
-    //         return {
-    //             icon: "plus",
-    //             label: `test ${index + 1}`,
-    //             onPress: () => "test 1",
-    //             color: "#fff",
-    //             style: {
-    //                 backgroundColor: "yellow",
-    //                 borderRadius: 20,
-    //             },
-    //         };
-    //     });
-    //     const tree: any = renderer.create(<FAB />).toJSON();
-    // });
-
 });
