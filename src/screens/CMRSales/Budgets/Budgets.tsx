c09233123ac4f9cb03ba20d05660bc905f94e557
Johan Román <r.johan95@gmail.com>
2024-05-27 21:03:47 -0400

    carga de scroll ilimitado de prueba

--- src/screens/CMRSales/Budgets/Budgets.tsx
+++ src/screens/CMRSales/Budgets/Budgets.tsx
@@ -76,9 +76,21 @@
 
 // const styles = StyleSheet.create({});
 
-import React, { useState } from "react";
-import { Platform, ScrollView, StyleSheet, View } from "react-native";
-import { DefaultTheme, Text, useTheme } from "react-native-paper";
+import React, { useEffect, useState } from "react";
+import {
+    FlatList,
+    ListRenderItem,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    View,
+} from "react-native";
+import {
+    ActivityIndicator,
+    DefaultTheme,
+    Provider,
+    useTheme,
+} from "react-native-paper";
 import { TextInput } from "react-native-paper";
 import { FontAwesome } from "@expo/vector-icons";
 import BudgetsCard from "../../../components/BudgetsCard/BudgetsCard";
@@ -96,6 +108,10 @@ export default function Budgets({ navigation }: { navigation: any }) {
     const [text, setText] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(false);
     const [showModal, setShowModal] = useState<boolean>(false);
+    const [data, setData] = useState<any[]>([]);
+    const [isRefreshing, setIsRefreshing] = useState<boolean>(false);
+    const totalPages = 5;
+    const [currentPage, setCurrentPage] = useState(1);
 
     const styles = StyleSheet.create({
         container: {
@@ -118,25 +134,81 @@ export default function Budgets({ navigation }: { navigation: any }) {
         cardsContainer: { marginVertical: 20 },
     });
 
+    /**
+     * Function to fetch data
+     * @returns
+     */
     function searchBudgets() {
         if (loading) return;
         setLoading(true);
 
         setTimeout(() => {
+            const newData = [...Array(1)].map((d, index) => {
+                const id = index + 1 + (currentPage - 1) * 1;
+                return {
+                    code: `2024-001.4${id}`,
+                    description: `${text} FOTOVOLTAICA AUTOCONSUMO [DEMO] [REVISION 300424] ${id}`,
+                    status: "Contratado",
+                    costo: "4.400,00",
+                    venta: "7.100,00",
+                };
+            });
+            setData((prevData) => [...prevData, ...newData]);
             setLoading(false);
-        }, 2000);
+        }, 3000);
     }
 
-    const totalPages = 5;
-    const [currentPage, setCurrentPage] = useState(1);
+    /**
+     * Fetch data when the component mounts or the page changes
+     */
+    useEffect(() => {
+        searchBudgets();
+    }, [currentPage]);
 
-    const handlePageChange = (page: number) => {
-        setCurrentPage(page);
-        // Fetch or update the data to be displayed based on the new page
-        console.log("Current Page:", page);
+    /**
+     * Handle loading more data when the end of the list is reached
+     */
+    const handleLoadMore = () => {
+        if (!loading) {
+            setCurrentPage((prevPage) => prevPage + 1);
+        }
     };
 
-    function handleLimitPerPage() {}
+    /**
+     * Handle refreshing the list
+     */
+    const handleRefresh = () => {
+        setIsRefreshing(true);
+        setCurrentPage(1);
+        setData([]);
+        searchBudgets();
+        setIsRefreshing(false);
+    };
+
+    // Render the loading indicator at the bottom of the list
+    const renderFooter = () => {
+        return loading ? (
+            <View /* style={styles.loader} */>
+                <ActivityIndicator size="large" />
+            </View>
+        ) : null;
+    };
+
+    /**
+     * Render an item in the list
+     *
+     * @param param0
+     * @returns
+     */
+    const renderItem: ListRenderItem<any> = ({ item }) => (
+        <BudgetsCard
+            index={item.code}
+            description={item.description}
+            status={item.status}
+            costo={item.costo}
+            venta={item.venta}
+        />
+    );
 
     return (
         <View style={styles.container}>
@@ -152,24 +224,25 @@ export default function Budgets({ navigation }: { navigation: any }) {
                         <TextInput.Icon
                             disabled={loading}
                             icon="magnify"
-                            onPress={() => searchBudgets()}
+                            onPress={() => handleRefresh()}
                             color={theme.colors.primary}
                         />
                     }
                 />
             </View>
 
-            <ScrollView style={{ marginVertical: 10 }}>
-                <BudgetsCard
-                    index={"2024-001.4"}
-                    description={
-                        "FOTOVOLTAICA AUTOCONSUMO [DEMO] [REVISION 300424]"
-                    }
-                    status={"Contratado"}
-                    costo={"4.400,00"}
-                    venta={"7.100,00"}
+            <View style={{ marginVertical: 10, flex: 1 }}>
+                <FlatList
+                    data={data}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={renderItem}
+                    onEndReached={handleLoadMore}
+                    onEndReachedThreshold={0.5}
+                    ListFooterComponent={renderFooter}
+                    refreshing={isRefreshing}
+                    onRefresh={handleRefresh}
                 />
-            </ScrollView>
+            </View>
 
             <Modal
                 visible={showModal}
@@ -182,35 +255,32 @@ export default function Budgets({ navigation }: { navigation: any }) {
             >
                 <CreateBudget setShowModal={() => setShowModal(!showModal)} />
             </Modal>
-            <FAB
-                actions={[
-                    {
-                        icon: "plus",
-                        label: t("menu-title-create-budget"),
-                        onPress: () => setShowModal(!showModal),
-                        color: theme.colors.primary,
-                        style: {
-                            backgroundColor: theme.colors.primaryContrast,
-                            borderRadius: 20,
+            <View style={{ flex: 1, marginTop: "-150%" }}>
+                <FAB
+                    actions={[
+                        {
+                            icon: "plus",
+                            label: t("menu-title-create-budget"),
+                            onPress: () => setShowModal(!showModal),
+                            color: theme.colors.primary,
+                            style: {
+                                backgroundColor: theme.colors.primaryContrast,
+                                borderRadius: 20,
+                            },
                         },
-                    },
-                    {
-                        icon: "file-export",
-                        label: t("export"),
-                        onPress: () => console.log("Pressed star"),
-                        color: theme.colors.primary,
-                        style: {
-                            backgroundColor: theme.colors.primaryContrast,
-                            borderRadius: 20,
+                        {
+                            icon: "file-export",
+                            label: t("export"),
+                            onPress: () => console.log("Pressed star"),
+                            color: theme.colors.primary,
+                            style: {
+                                backgroundColor: theme.colors.primaryContrast,
+                                borderRadius: 20,
+                            },
                         },
-                    },
-                ]}
-            ></FAB>
-            <Paginator
-                totalPages={totalPages}
-                onPageChange={handlePageChange}
-                onLimitChange={handleLimitPerPage}
-            />
+                    ]}
+                ></FAB>
+            </View>
         </View>
     );
 }
