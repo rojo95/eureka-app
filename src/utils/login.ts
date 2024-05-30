e5e6c49496d8f22877f84f428cd009ed4b881705
Johan Román <r.johan95@gmail.com>
2024-05-30 01:55:52 -0400

    depuracion de codigo

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 import Constants from "expo-constants";
 import * as SecureStore from "expo-secure-store";
-import { Platform } from "react-native";
 import sessionNames from "./sessionInfo";
 import { getUserData } from "../services/users/users";
-import { getSecureData, saveSecureData } from "../services/storeData/storeData";
+import { saveSecureData } from "../services/storeData/storeData";
 
 const constants = Constants.expoConfig?.extra;
 
@@ -16,12 +15,6 @@ export interface LoginProps {
 }
 
 const API_URL = constants?.API_URL;
-
-interface SecureStoreInterface {
-    key: string;
-    value: string;
-}
-const { OS } = Platform;
 
 /**
  * function to log in to the system api
@@ -47,7 +40,6 @@ export async function login({ email, password }: LoginProps) {
         .then(async ({ request }) => {
             const { id, userId } = JSON.parse(request.response);
             if (id) {
-                console.log("Login successful!");
                 await saveSecureData({ key: userKey, value: id });
                 await saveSecureData({ key: idUser, value: userId });
 
