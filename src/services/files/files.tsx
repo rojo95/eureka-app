5baa344fb67d87c8f7ca99f6bef371cbe1707160
Johan Román <r.johan95@gmail.com>
2024-06-01 20:51:32 -0400

    creacion de funcion para descargar archivos (incompleta)

---
+++ src/services/files/files.tsx
@@ -0,0 +1,11 @@
+export async function downLoadDocument({
+    documentName,
+    url,
+}: {
+    documentName: string;
+    url: URL;
+}) {
+    console.log(url);
+
+    // todo should make the functionality to donwload the document at the phone or the pc
+}
